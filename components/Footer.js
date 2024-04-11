fc65bacf187d344ac7580a6846a434355b3d0ff6
Manish Kumar <manishjune27@gmail.com>
2024-04-12 01:13:01 +0530

    made cart work

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
-const Footer = ()=>{
-    return(
-        <div className="dark:border-t-[0.01rem] p-6 flex justify-center items-center font-semibold text-[#334155] shadow-inner shadow-slate-100 dark:shadow-none backdrop-blur-3xl  dark:border-[#303235] dark:text-[#e2e8f0] mt-8">Made in INDIA</div>
-    );
-}
+const Footer = () => {
+  return (
+    <div className="mt-8 flex items-center justify-center p-6 font-semibold text-[#334155] shadow-inner shadow-slate-100 backdrop-blur-3xl dark:border-t-[0.01rem]  dark:border-[#303235] dark:text-[#e2e8f0] dark:shadow-none">
+      Made in INDIA
+    </div>
+  );
+};
 
 export default Footer;
